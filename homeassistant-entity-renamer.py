59bf0d7c6332ba6c7e987d965d4ecb2502b61703
Coenenp <pieter.coenen@gmail.com>
2024-05-11 08:12:50 +0700

    Update homeassistant-entity-renamer.py

--- homeassistant-entity-renamer.py
+++ homeassistant-entity-renamer.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import argparse
 import re
 import requests
@@ -19,6 +17,7 @@ headers = {
     'Content-Type': 'application/json'
 }
 
+
 def align_strings_in_column(table, column, c="."):
     # Get the column data from the table
     column_data = [row[column] for row in table]
@@ -35,8 +34,8 @@ def align_strings_in_column(table, column, c="."):
         tuple(align_string(value) if i == column else value for i, value in enumerate(row))
         for row in table
     ]
-
     return modified_table
+
 
 def list_entities(regex=None):
     # API endpoint for retrieving all entities
@@ -77,6 +76,7 @@ def write_to_csv(entity_data, filename):
         for row in entity_data:
             writer.writerow(row)
 
+
 def rename_entities(entity_data, search_regex, replace_regex):
     renamed_data = []
     for friendly_name, entity_id in entity_data:
@@ -93,7 +93,7 @@ def rename_entities(entity_data, search_regex, replace_regex):
     # Ask user for confirmation if replace_regex is provided
     if replace_regex:
         answer = input("\nDo you want to proceed with renaming the entities? (y/N): ")
-        if answer.lower() == "y" or answer.lower() == "yes":
+        if answer.lower() in ["y", "yes"]:
             websocket_url = f'ws{TLS_S}://{config.HOST}/api/websocket'
             ws = websocket.WebSocket()
             ws.connect(websocket_url)
@@ -130,16 +130,63 @@ def rename_entities(entity_data, search_regex, replace_regex):
             print("Renaming process aborted.")
 
 
+def update_entity_friendly_names(input_filename):
+    answer = input("\nDo you want to proceed with renaming the entities? (y/N): ")
+    if answer.lower() in ["y", "yes"]:
+        websocket_url = f'ws{TLS_S}://{config.HOST}/api/websocket'
+        ws = websocket.WebSocket()
+        ws.connect(websocket_url)
+
+        auth_req = ws.recv()
+
+        # Authenticate with Home Assistant
+        auth_msg = json.dumps({"type": "auth", "access_token": config.ACCESS_TOKEN})
+        ws.send(auth_msg)
+        auth_result = ws.recv()
+        auth_result = json.loads(auth_result)
+        if auth_result["type"] != "auth_ok":
+            print("Authentication failed. Check your access token.")
+            return
+
+        # Update friendly names
+        updated_data = []
+        with open(input_filename, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                entity_id = row['Entity ID']
+                friendly_name = row['Friendly Name']
+                updated_data.append((friendly_name, entity_id))
+
+        for index, (friendly_name, entity_id) in enumerate(updated_data, start=1):
+            # Update entity friendly name
+            entity_registry_update_msg = json.dumps({
+                "id": index,
+                "type": "config/entity_registry/update",
+                "entity_id": entity_id,
+                "name": friendly_name
+            })
+            ws.send(entity_registry_update_msg)
+            update_result = ws.recv()
+            update_result = json.loads(update_result)
+            if update_result["success"]:
+                print(f"Entity '{entity_id}' renamed to '{friendly_name}' successfully!")
+            else:
+                print(f"Failed to update entity '{entity_id}': {update_result['error']['message']}")
+
+        ws.close()
+    else:
+        print("Renaming process aborted.")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="HomeAssistant Entity Renamer")
+    parser.add_argument('--input-file', dest='input_filename', default='input.csv', help='Input CSV file containing Friendly Name and Entity ID')
     parser.add_argument('--search', dest='search_regex', help='Regular expression for search. Note: Only searches entity IDs.')
     parser.add_argument('--replace', dest='replace_regex', help='Regular expression for replace')
     args = parser.parse_args()
 
     if args.search_regex:
-        entity_data = list_entities(args.search_regex)
-
-        if entity_data:
+        if entity_data := list_entities(args.search_regex):
             if args.replace_regex:
                 rename_entities(entity_data, args.search_regex, args.replace_regex)
             else:
@@ -150,5 +197,9 @@ if __name__ == "__main__":
                 write_to_csv(entity_data, "output.csv")
         else:
             print("No entities found matching the search regex.")
+    elif args.input_filename:
+        input_filename = args.input_filename
+
+        update_entity_friendly_names(input_filename)
     else:
         parser.print_help()
